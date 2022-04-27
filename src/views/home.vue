5db531cd2f89e56dbecff25220a09e1ad5fb53e5
MALISETTYSUMANTH02 <sumanth.malisetty@publicissapient.com>
2022-04-27 17:01:13 +0530

    ch

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,7 +8,7 @@
       ></Icon>
     </div>
 
-    <div v-if="showMenu" class="bg-gray-900 w-full p-5 fixed flex text-white">
+    <div v-if="showMenu" class="bg-blue-600 w-full p-5 fixed flex text-white">
       <div class="">
         <RouterLink :to="'/pokemons/'" class="font-bold m-2 block"
           >Pokemons/Pokedox</RouterLink
@@ -23,15 +23,15 @@
     </div>
 
     <div className="text-center text-xl py-10 font-mono  pt-30 ">
-      <h1 className="text-4xl font-semibold text-cyan-300">
-        PW FRONTEND ASSSIGNMENT
+      <h1 className="text-4xl font-bold text-red-500">
+        Physicswallah FRONTEND ASSSIGNMENT
       </h1>
       <div className="p-4">
-        1. Main Page : Get a List of Pokemons. Add Pagination referring to
+        1. Main Page - Get a List of Pokemons. Add Pagination referring to
         original docs.
       </div>
       <div className="p-4">
-        2. Pokemon Page: opens via clicking on respective pokemon.
+        2. Pokemon Page - opens via clicking on respective pokemon.
       </div>
     </div>
   </div>
